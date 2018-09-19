6144f5d8cc180892ffe2b13890c3ac7580bd788d
srividhya29 <43168219+srividhya29@users.noreply.github.com>
2018-09-19 06:34:35 +0530

    Calculator in windows forms

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.ndot = new System.Windows.Forms.Button();
+            this.bc = new System.Windows.Forms.Button();
+            this.nequal = new System.Windows.Forms.Button();
+            this.bmul = new System.Windows.Forms.Button();
+            this.bad = new System.Windows.Forms.Button();
+            this.bsub = new System.Windows.Forms.Button();
+            this.bdiv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(6, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(353, 43);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+           
+            // 
+            // n1
+            // 
+            this.n1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.n1.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n1.Location = new System.Drawing.Point(29, 78);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(87, 73);
+            this.n1.TabIndex = 1;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = true;
+            this.n1.Click += new System.EventHandler(this.click);
+            // 
+            // n2
+            // 
+            this.n2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.n2.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n2.Location = new System.Drawing.Point(123, 78);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(96, 73);
+            this.n2.TabIndex = 2;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = true;
+            this.n2.Click += new System.EventHandler(this.click);
+            // 
+            // n3
+            // 
+            this.n3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.n3.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n3.Location = new System.Drawing.Point(229, 78);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(96, 73);
+            this.n3.TabIndex = 3;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = true;
+            this.n3.Click += new System.EventHandler(this.click);
+            // 
+            // n4
+            // 
+            this.n4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.n4.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n4.Location = new System.Drawing.Point(29, 157);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(88, 69);
+            this.n4.TabIndex = 4;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = true;
+            this.n4.Click += new System.EventHandler(this.click);
+            // 
+            // n5
+            // 
+            this.n5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.n5.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n5.Location = new System.Drawing.Point(123, 157);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(96, 69);
+            this.n5.TabIndex = 5;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = true;
+            this.n5.Click += new System.EventHandler(this.click);
+            // 
+            // n6
+            // 
+            this.n6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.n6.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n6.Location = new System.Drawing.Point(229, 153);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(96, 69);
+            this.n6.TabIndex = 6;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = true;
+            this.n6.Click += new System.EventHandler(this.click);
+            // 
+            // n7
+            // 
+            this.n7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.n7.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n7.Location = new System.Drawing.Point(32, 232);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(88, 57);
+            this.n7.TabIndex = 7;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = true;
+            this.n7.Click += new System.EventHandler(this.click);
+            // 
+            // n8
+            // 
+            this.n8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.n8.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n8.Location = new System.Drawing.Point(126, 232);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(93, 57);
+            this.n8.TabIndex = 8;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = true;
+            this.n8.Click += new System.EventHandler(this.click);
+            // 
+            // n9
+            // 
+            this.n9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.n9.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n9.Location = new System.Drawing.Point(229, 230);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(96, 59);
+            this.n9.TabIndex = 9;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = true;
+            this.n9.Click += new System.EventHandler(this.click);
+            // 
+            // n0
+            // 
+            this.n0.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.n0.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n0.Location = new System.Drawing.Point(229, 295);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(96, 57);
+            this.n0.TabIndex = 10;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = true;
+            this.n0.Click += new System.EventHandler(this.click);
+            // 
+            // ndot
+            // 
+            this.ndot.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ndot.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ndot.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ndot.Location = new System.Drawing.Point(32, 295);
+            this.ndot.Name = "ndot";
+            this.ndot.Size = new System.Drawing.Size(88, 57);
+            this.ndot.TabIndex = 11;
+            this.ndot.Text = ".";
+            this.ndot.UseVisualStyleBackColor = true;
+            this.ndot.Click += new System.EventHandler(this.ndot_Click);
+            // 
+            // bc
+            // 
+            this.bc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bc.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bc.Location = new System.Drawing.Point(229, 471);
+            this.bc.Name = "bc";
+            this.bc.Size = new System.Drawing.Size(88, 57);
+            this.bc.TabIndex = 12;
+            this.bc.Text = "C";
+            this.bc.UseVisualStyleBackColor = true;
+            this.bc.Click += new System.EventHandler(this.bc_Click);
+            // 
+            // nequal
+            // 
+            this.nequal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nequal.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nequal.Location = new System.Drawing.Point(32, 393);
+            this.nequal.Name = "nequal";
+            this.nequal.Size = new System.Drawing.Size(88, 57);
+            this.nequal.TabIndex = 13;
+            this.nequal.Text = "=";
+            this.nequal.UseVisualStyleBackColor = true;
+            this.nequal.Click += new System.EventHandler(this.nequal_Click);
+            // 
+            // bmul
+            // 
+            this.bmul.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bmul.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bmul.Location = new System.Drawing.Point(131, 393);
+            this.bmul.Name = "bmul";
+            this.bmul.Size = new System.Drawing.Size(88, 57);
+            this.bmul.TabIndex = 14;
+            this.bmul.Text = "*";
+            this.bmul.UseVisualStyleBackColor = true;
+            this.bmul.Click += new System.EventHandler(this.onClickOperator);
+            // 
+            // bad
+            // 
+            this.bad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bad.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bad.Location = new System.Drawing.Point(229, 393);
+            this.bad.Name = "bad";
+            this.bad.Size = new System.Drawing.Size(88, 57);
+            this.bad.TabIndex = 15;
+            this.bad.Text = "+";
+            this.bad.UseVisualStyleBackColor = true;
+            this.bad.Click += new System.EventHandler(this.onClickOperator);
+            // 
+            // bsub
+            // 
+            this.bsub.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bsub.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bsub.Location = new System.Drawing.Point(32, 471);
+            this.bsub.Name = "bsub";
+            this.bsub.Size = new System.Drawing.Size(88, 57);
+            this.bsub.TabIndex = 16;
+            this.bsub.Text = "-";
+            this.bsub.UseVisualStyleBackColor = true;
+            this.bsub.Click += new System.EventHandler(this.onClickOperator);
+            // 
+            // bdiv
+            // 
+            this.bdiv.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bdiv.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bdiv.Location = new System.Drawing.Point(131, 471);
+            this.bdiv.Name = "bdiv";
+            this.bdiv.Size = new System.Drawing.Size(88, 57);
+            this.bdiv.TabIndex = 17;
+            this.bdiv.Text = "/";
+            this.bdiv.UseVisualStyleBackColor = true;
+            this.bdiv.Click += new System.EventHandler(this.onClickOperator);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 560);
+            this.Controls.Add(this.bdiv);
+            this.Controls.Add(this.bsub);
+            this.Controls.Add(this.bad);
+            this.Controls.Add(this.bmul);
+            this.Controls.Add(this.nequal);
+            this.Controls.Add(this.bc);
+            this.Controls.Add(this.ndot);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n3;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.Button ndot;
+        private System.Windows.Forms.Button bc;
+        private System.Windows.Forms.Button nequal;
+        private System.Windows.Forms.Button bmul;
+        private System.Windows.Forms.Button bad;
+        private System.Windows.Forms.Button bsub;
+        private System.Windows.Forms.Button bdiv;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        double FirstNumber;
+        string Operation;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void click(object sender,EventArgs e)
+        {
+            Button b = sender as Button;
+            
+            if (textBox1.Text == "0" && textBox1.Text != null && b.Text !="0")
+            {
+                textBox1.Text = b.Text;
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + b.Text;
+            }
+           
+
+        }
+
+        private void onClickOperator(object sender, EventArgs e)
+        {
+           
+            Button b = sender as Button;
+            if(b.Text == "+")
+            {
+                FirstNumber = Convert.ToDouble(textBox1.Text);
+                textBox1.Text = "0";
+                Operation = "+";
+            }
+            else if(b.Text == "-")
+            {
+                FirstNumber = Convert.ToDouble(textBox1.Text);
+                textBox1.Text = "0";
+                Operation = "-";
+            }
+            else if(b.Text == "*")
+            {
+                FirstNumber = Convert.ToDouble(textBox1.Text);
+                textBox1.Text = "0";
+                Operation = "*";
+
+            }
+
+            else if(b.Text == "/")
+            {
+                FirstNumber = Convert.ToDouble(textBox1.Text);
+                textBox1.Text = "0";
+                Operation = "/";
+            }
+        }
+
+       
+
+        private void bc_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+
+
+        Boolean callFirst = false;
+        private void ndot_Click(object sender, EventArgs e)
+        {
+            if(callFirst == false)
+            {
+                textBox1.Text = textBox1.Text + ".";
+                callFirst = true;
+
+            }
+           
+        }
+
+       
+
+        private void nequal_Click(object sender, EventArgs e)
+        {
+            double SecondNumber;
+            double Result;
+
+            SecondNumber = Convert.ToDouble(textBox1.Text);
+
+            if (Operation == "+")
+            {
+                Result = (FirstNumber + SecondNumber);
+                textBox1.Text = Convert.ToString(Result);
+                FirstNumber = Result;
+            }
+            if (Operation == "-")
+            {
+                Result = (FirstNumber - SecondNumber);
+                textBox1.Text = Convert.ToString(Result);
+                FirstNumber = Result;
+            }
+            if (Operation == "*")
+            {
+                Result = (FirstNumber * SecondNumber);
+                textBox1.Text = Convert.ToString(Result);
+                FirstNumber = Result;
+            }
+            if (Operation == "/")
+            {
+                if (SecondNumber == 0)
+                {
+                    textBox1.Text = "Cannot divide by zero";
+
+                }
+                else
+                {
+                    Result = (FirstNumber / SecondNumber);
+                    textBox1.Text = Convert.ToString(Result);
+                    FirstNumber = Result;
+                }
+            }
+        }
+    }
+}
